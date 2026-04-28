1e77d089c17afa0b33889a378171cea9656cff5d
ricsaagon <a21ricsaagon@inspedralbes.cat>
2026-04-28 14:22:29 +0200

    Creacio de pagina de formulari per crear una incidencia

---
+++ src/insertar.php
@@ -0,0 +1,33 @@
+<div class="row">
+    <div class="col-12">
+        <h1>Registrar videojuego</h1>
+        <form action="registrar.php" method="POST">
+            
+            <!-- Descripció -->
+            <div class="form-group">
+                <label for="descripcio">Descripcio</label>
+                <textarea placeholder="Descripció" class="form-control" name="descripcio" id="descripcio" cols="30" rows="10" required></textarea>
+            </div>
+            
+
+            <!-- Tipus -->
+            <div class="form-group">
+                <label>ID Tipo</label>
+                <input type="number" name="idTipus" class="form-control" required>
+            </div>
+
+            <!-- DEpartament -->
+            <div class="form-group">
+                <label>ID Departamento</label>
+                <input type="number" name="idDepartament" class="form-control" required>
+            </div>
+
+
+
+            <div class="form-group mt-3">
+                <button class="btn btn-success">Guardar</button>
+            </div>
+        </form>
+    </div>
+</div>
+<?php include_once "pie.php"; ?>
